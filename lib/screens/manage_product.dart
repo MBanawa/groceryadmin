f66ad5cd78e4742adb411980e6e34a28c03aa7a1
MBanawa <mikebanawa25@gmail.com>
2022-03-15 21:26:52 +0800

    added save product function

--- lib/screens/manage_product.dart
+++ lib/screens/manage_product.dart
@@ -14,6 +14,10 @@ class _ManageProductScreenState extends State<ManageProductScreen> {
   var _selectedId = "Dduq5nSc8LZX9QDZnorj";
   final FirebaseFirestore _db = FirebaseFirestore.instance;
 
+  final TextEditingController _titleCtrl = TextEditingController();
+  final TextEditingController _priceCtrl = TextEditingController();
+  final TextEditingController _desCtrl = TextEditingController();
+
   fetchCategories() {
     _db.collection("categories").snapshots().listen((event) {
       var _tmp = [];
@@ -29,9 +33,19 @@ class _ManageProductScreenState extends State<ManageProductScreen> {
     });
   }
 
+  add() {
+    _db.collection("products").add({
+      "title": _titleCtrl.text,
+      "price": _priceCtrl.text,
+      "categoryId": _selectedId,
+      "desc": _desCtrl.text
+    }).then((value) {
+      Get.back();
+    });
+  }
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     fetchCategories();
   }
@@ -39,90 +53,94 @@ class _ManageProductScreenState extends State<ManageProductScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       appBar: AppBar(
-        title: Text("Manage Product"),
+        title: const Text("Manage Product"),
       ),
-      body: Container(
-        padding: EdgeInsets.all(32.0),
-        width: double.infinity,
-        height: double.infinity,
-        child: Column(
-          children: [
-            CircleAvatar(
-              backgroundImage: AssetImage("assets/images/profile.png"),
-              radius: 60,
-            ),
-            SizedBox(height: 40),
-            TextField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.grey[200],
-                border: InputBorder.none,
-                labelText: "Product Title",
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            children: [
+              const CircleAvatar(
+                backgroundImage: AssetImage("assets/images/profile.png"),
+                radius: 60,
               ),
-            ),
-            SizedBox(height: 12),
-            TextField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.grey[200],
-                border: InputBorder.none,
-                labelText: "Price",
-              ),
-            ),
-            SizedBox(height: 12),
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text("Category"),
-                  DropdownButton(
-                    value: _selectedId,
-                    items: _categories.map((category) {
-                      return DropdownMenuItem(
-                        child: Text(category["title"]),
-                        value: category["id"],
-                      );
-                    }).toList(),
-                    onChanged: (v) {
-                      setState(() {
-                        _selectedId = v.toString();
-                      });
-                    },
-                  )
-                ],
-              ),
-            ),
-            TextField(
-              maxLines: 4,
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.grey[200],
-                border: InputBorder.none,
-                labelText: "Description",
-              ),
-            ),
-            SizedBox(height: 12),
-            Container(
-              width: double.infinity,
-              height: 48,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  elevation: 0,
-                  primary: Colors.green,
+              const SizedBox(height: 40),
+              TextField(
+                controller: _titleCtrl,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: InputBorder.none,
+                  labelText: "Product Title",
                 ),
-                child: Text(
-                  "Save Changes",
-                  style: TextStyle(
-                    fontSize: 16,
+              ),
+              const SizedBox(height: 12),
+              TextField(
+                controller: _priceCtrl,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: InputBorder.none,
+                  labelText: "Price",
+                ),
+              ),
+              const SizedBox(height: 12),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text("Category"),
+                    DropdownButton(
+                      value: _selectedId,
+                      items: _categories.map((category) {
+                        return DropdownMenuItem(
+                          child: Text(category["title"]),
+                          value: category["id"],
+                        );
+                      }).toList(),
+                      onChanged: (v) {
+                        setState(() {
+                          _selectedId = v.toString();
+                        });
+                      },
+                    )
+                  ],
+                ),
+              ),
+              TextField(
+                controller: _desCtrl,
+                maxLines: 4,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: InputBorder.none,
+                  labelText: "Description",
+                ),
+              ),
+              const SizedBox(height: 12),
+              Container(
+                width: double.infinity,
+                height: 48,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    elevation: 0,
+                    primary: Colors.green,
                   ),
+                  child: const Text(
+                    "Save Changes",
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                  onPressed: () {
+                    add();
+                  },
                 ),
-                onPressed: () {
-                  Get.back();
-                },
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
